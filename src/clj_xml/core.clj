f9f2a494a1a3a2d02ab88fa1c5a6d682e2263c67
mchoi <hello@mandychoi.com.au>
2021-05-19 01:51:20 +0100

    Add doc-string to public functions

--- src/clj_xml/core.clj
+++ src/clj_xml/core.clj
@@ -5,15 +5,21 @@
   (:import (javax.xml.parsers SAXParserFactory)))
 
 (defn get-all-content [xml-zipper tag]
+  "Returns an array of contents of all the elements found for a given tag."
+  {:added "0.1.0"}
   (->> xml-zipper
        (filter #(= (:tag %) tag))
        (map :content)
        (flatten)))
 
-(defn get-content-in [xml-zipper ks]
+(defn get-content-in [xml-zipper tags]
+  "Returns the nested content in a xml elements zipper,
+   where ks is a sequence of tags. Returns nil if the tag
+   is not present."
+  {:added "0.1.0"}
   (first (reduce get-all-content
                  xml-zipper
-                 ks)))
+                 tags)))
 
 (declare newSAXParser parse)
 (defn- non-validating [s ch]
@@ -26,6 +32,9 @@
     (parse s ch)))
 
 (defn xml-str->zipper [xml-str]
+  "Returns a zipper for xml elements (as from xml/parse, each
+   containing :tag, :attrs and :content) for a given a xml string"
+  {:added "0.1.0"}
   (-> xml-str
       (.getBytes)
       (io/input-stream)
